a34d530fdaf92f4f3a17554a0c2c0a710023a5ce
Brmuus14 <luke.barkess@icloud.com>
2020-01-08 18:21:49 +0000

    epic
    made simle level no jumping tho see project manager

---
+++ PowerRush/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    void FixedUpdate()
+    {
+        Vector2 playerPos = transform.position;
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            playerPos.x += moveSpeed * Time.deltaTime;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            playerPos.x -= moveSpeed * Time.deltaTime;
+        }
+        transform.position = playerPos;
+    }
+}
