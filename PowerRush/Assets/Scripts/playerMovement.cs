f0c69430cd8488fcfa4a07bd42621523087ebfb2
Brmuus14 <luke.barkess@icloud.com>
2020-01-12 17:03:45 +0000

    issue
    player can jump infinitly need to only jump in contact with the floor.

--- PowerRush/Assets/Scripts/playerMovement.cs
+++ PowerRush/Assets/Scripts/playerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    [SerializeField] private LayerMask LevelLayerMask;
     private Rigidbody2D rigidbody2d;
-    private BoxCollider2D boxcollider2d;
+    private BoxCollider2D boxCollider2d;
     public float moveSpeed = 3f;
     private void Awake()
     {
         rigidbody2d = transform.GetComponent<Rigidbody2D>();
-        boxcollider2d = transform.GetComponent<BoxCollider2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
     }
-    void FixedUpdate()
+
+    private void Update()
+    {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 10f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down * 0.1f, LevelLayerMask);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
+    }   
+    private void FixedUpdate()
     {
         Vector2 playerPos = transform.position;
         if (Input.GetAxisRaw("Horizontal") > 0)
@@ -24,17 +40,6 @@ public class playerMovement : MonoBehaviour
         {
             playerPos.x -= moveSpeed * Time.deltaTime;
         }
-
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            float jumpVelocity = 10f;
-            rigidbody2d.velocity = Vector2.up * jumpVelocity;
-        }
         transform.position = playerPos;
-
-        private bool IsGrounded()
-        {
-            Physics2D.BoxCast(boxcollider2d.bounds.center, boxcollider2d.bounds.size, 0f, Vector2.down * .1f);
-        }
     }
 }
