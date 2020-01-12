2170acd7ac4babc38dc06cee95a15b14bcd4ca00
Brmuus14 <luke.barkess@icloud.com>
2020-01-12 13:00:57 +0000

    bruh
    efjwjnuebvbgyuenguhrfufnu

---
+++ PowerRush/Assets/Scripts/playerCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player Hit");
+        Time.timeScale = 0f;
+        Destroy(gameObject);
+    }
+}
--- PowerRush/Assets/Scripts/playerMovement.cs
+++ PowerRush/Assets/Scripts/playerMovement.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxcollider2d;
     public float moveSpeed = 3f;
+    private void Awake()
+    {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxcollider2d = transform.GetComponent<BoxCollider2D>();
+    }
     void FixedUpdate()
     {
         Vector2 playerPos = transform.position;
@@ -17,6 +24,17 @@ public class playerMovement : MonoBehaviour
         {
             playerPos.x -= moveSpeed * Time.deltaTime;
         }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 10f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
         transform.position = playerPos;
+
+        private bool IsGrounded()
+        {
+            Physics2D.BoxCast(boxcollider2d.bounds.center, boxcollider2d.bounds.size, 0f, Vector2.down * .1f);
+        }
     }
 }
